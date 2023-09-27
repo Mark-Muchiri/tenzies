e2f662112524e653c1c4a636cda98032c5a239b4
Mark <muchirimark2@gmail.com>
2023-09-28 00:00:34 +0300

    working on tenzies styling using css modules

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Tenzies from './components/App.js';
+// 'use client'
+import styles from './page.module.css';
 
 function Home() {
+  /**
+  * Challenge: Start a brand new React app!
+  * - ✅ Create a separate App component
+  * - ✅ Import and render the App component here
+  * - ✅ In the App component, render a <main> element
+  * - Style everything to look like the slide
+  */
   return (
-    <main>
-      <Tenzies />
+    <main className={styles.background}>
+      <div className={styles.box}>
+        <h2 className={styles.title}>Tenzies</h2>
+      </div>
     </main>
   );
 }
