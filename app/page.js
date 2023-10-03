9fa32a253471b00989d23f40b36e031aa4534f28
Mark <muchirimark2@gmail.com>
2023-10-03 21:52:08 +0300

    Solved Challenge:
    	 * Updated the `rollDice` function to not just roll all new dice, but
         instead to look through the existing dice to NOT role any that
         are being `held`.
    	 * Hint: this will look relatively similiar to the `holdDice` function
         below.
       * When creating new dice, remember to use `id: nanoid()` so any new
         dice have an `id` as well.
    Changes to be committed:
    	modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -4,35 +4,33 @@ import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Dice from "./dice.js";
 
-function Home({ initialValue }) {
+function Home() {
 	// Initialize the component state with a the `allNewDice`
-	const [dice, setDice] = useState(allNewDice() || initialValue);
-	// This logic is used to  provide initial load values of the dice
-	function initialDice() {
-		const newDice = [];
-		for (let i = 0; i < 10; i++) {
-			const randomNumber = Math.ceil(Math.random() * 6);
-			newDice.push({
-				id: nanoid,
-				value: randomNumber,
-				isHeld: false,
-			});
-		}
-		return newDice;
+	const [ dice, setDice ] = useState(allNewDice());
+
+	function generateNewDie() {
+		return {
+			id: nanoid(),
+			value: Math.ceil(Math.random() * 6),
+			isHeld: false,
+		};
 	}
 
 	// This logic is only executed on the client side
 	function allNewDice() {
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
-			const randomNumber = Math.ceil(Math.random() * 6);
-			newDice.push({
-				id: nanoid(),
-				value: randomNumber,
-				isHeld: false,
-			});
+			newDice.push(generateNewDie());
 		}
 		return newDice;
+	}
+
+	function holdDice(id) {
+		setDice((prevDice) =>
+			prevDice.map((box) =>
+				box.id === id ? { ...box, isHeld: !box.isHeld } : box
+			)
+		);
 	}
 
 	/**
@@ -44,19 +42,15 @@ function Home({ initialValue }) {
 	 * function below. When creating new dice, remember to use
 	 * `id: nanoid()` so any new dice have an `id` as well.
 	 */
-
-	function holdDice(id) {
-		setDice((prevDice) =>
-			prevDice.map((box) =>
-				box.id === id ? { ...box, isHeld: !box.isHeld } : box
-			)
-		);
-	}
-
 	// Roll dice button click function
 	function rollDice() {
 		// Generate new random numbers for dice and update the state
-		setDice(allNewDice());
+		// Also holds the the truthy for `isHeld`
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.isHeld ? die : generateNewDie();
+			})
+		);
 	}
 
 	// Map the dice values to Dice components
@@ -81,7 +75,10 @@ function Home({ initialValue }) {
 						freeze it at its current value between rolls.
 					</p>
 					<div className={styles.Dicegrid}>{numbers}</div>
-					<button className={styles.Roll} onClick={rollDice}>
+					<button
+						onClick={rollDice}
+						className={styles.Roll}
+					>
 						<p className={styles.RollText}>Roll</p>
 					</button>
 				</div>
