48dd0d1bf2df2f484b0bb9feb503a74f7fb556d1
Mark <muchirimark2@gmail.com>
2023-10-04 19:27:35 +0300

    null

--- app/dice.js
+++ app/dice.js
@@ -1,23 +1,21 @@
-import dice from './dice.module.css';
+import dice from "./dice.module.css";
 
 function Dice(props) {
-  const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "#707070"
-  };
-  return (
-    <section>
-      <main className={dice.grid}>
-        <div
-          onClick={props.toggle}
-          className={dice.cube}
-          style={styles}
-        >
-          <p className={dice.value}>
-            {props.value}
-          </p>
-        </div>
-      </main>
-    </section>
-  );
+	const styles = {
+		backgroundColor: props.isHeld ? "#59E391" : "#707070",
+	};
+	return (
+		<section>
+			<main className={dice.grid}>
+				<div
+					onClick={props.toggle}
+					className={dice.cube}
+					style={styles}
+				>
+					<p className={dice.value}>{props.value}</p>
+				</div>
+			</main>
+		</section>
+	);
 }
 export default Dice;
